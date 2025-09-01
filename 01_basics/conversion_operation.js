bf55b54959c5cb8e61ba2b906b1743b867e9c865
Mohid Bin Zubair <154825053+mohidzubair@users.noreply.github.com>
2025-09-01 16:20:57 +0000

    conversion of datatypes

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,21 @@
+let score = "33ab"
+
+console.log(typeof(score))
+
+console.log(typeof score )
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+console.log(valueInNumber)
+
+
+// '33' => 33
+// '33avs' => NaN
+// true => 1
+// 
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
