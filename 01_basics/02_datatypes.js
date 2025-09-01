2eaec25f37fbe2e0d2b82a9fd9f0081364619559
Mohid Bin Zubair <154825053+mohidzubair@users.noreply.github.com>
2025-09-01 16:02:00 +0000

    dataatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// alert(3 + 3) it will not work because we are using node.js not browser
+let age = 18
+console.log(3 + 4)
+console.log("Mohid Bin Zubair")
+
+console.log(typeof null) // object
