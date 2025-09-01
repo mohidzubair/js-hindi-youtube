eb84e247c2da68379e04e765826115ebeaac2989
Mohid Bin Zubair <154825053+mohidzubair@users.noreply.github.com>
2025-09-01 17:24:14 +0000

    conversion operations change

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -1,13 +1,13 @@
 let score = "33ab"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-console.log(typeof score )
+// console.log(typeof score )
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
+// console.log(typeof(valueInNumber))
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 
 // '33' => 33
@@ -18,4 +18,24 @@ console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// *********************Operations*******************
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Mohid"
+let str2 = "Zubair"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
